763854abcef6fc276ba49310390c9fa7a0360d6e
SWORDSYT <156536037+SWORDSYT@users.noreply.github.com>
2024-04-29 21:56:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
 
-global.owner = [['917605902011', 'GURU', true], ['6281255369012', 'Ash', true], ['']] //Number of owners
+global.owner = [['917605902011', 'GURU', true], ['6281255369012', 'Ash', true], ['9060187481']] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "GuruBot~WQVLQkQv" //put your bot number here
 
 global.mods = ['917605902011', '32460248586']
 global.prems = ['917605902011', '32460248586', '919398758484']
